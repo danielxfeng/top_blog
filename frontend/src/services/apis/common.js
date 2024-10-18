d9c37c3d0c420abbdceac1e2eb493b638f12d6a7
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-18 18:21:16 +0300

    FE: User api with test

---
+++ frontend/src/services/apis/common.js
@@ -0,0 +1,36 @@
+const host = "http://localhost:3000/api";
+
+// A helper function to parse the JSON response body.
+const parseResponse = async (response) => {
+  if (!response.ok)
+    throw new Error(`Error: ${response.status}, ${await response.json()}`);
+  if (response.status === 204) return {};
+  return response.json();
+};
+
+// A helper function to get the headers for the requests.
+// It includes the token if it exists in Local storage.
+const getHeaders = () => {
+  let token = {};
+  try {
+    const user = JSON.parse(localStorage.getItem("user"));
+    token = { Authorization: `Bearer ${user.token}` };
+  } catch {}
+  return {
+    "Content-Type": "application/json",
+    ...token,
+  };
+};
+
+// A helper function to get the query for fetching.
+const getQuery = (method, body = null) => {
+  const bodyData = body ? { body: JSON.stringify(body) } : {};
+  return {
+    mode: "cors",
+    method: method,
+    headers: getHeaders(),
+    ...bodyData,
+  };
+};
+
+export { host, parseResponse, getQuery };
