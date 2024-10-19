0869e423e0a80d2cfc321fe3b4aea5be08a612cf
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-19 21:35:44 +0300

    FE: Styled ErrorPage

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
 import { useRouteError, Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 // The ErrorPage displays an error message when an unexpected error occurs.
 // Either routeError which means the error occurred in the route,
@@ -12,15 +13,22 @@ const ErrorPage = ({ errMsg }) => {
   const msg = routeError ? routeError.statusText || routeError.message : errMsg;
 
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{msg}</i>
-      </p>
-      <p>
-        Back to <Link to="/">Home</Link>
-      </p>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 flex flex-col gap-5 items-center justify-center">
+        <h1 className="text-4xl text-gradient">Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{msg}</i>
+        </p>
+        <p>
+          Back to{" "}
+          <Link className="underline" to="/">
+            Home
+          </Link>
+        </p>
+      </main>
+      <Footer />
     </div>
   );
 };
