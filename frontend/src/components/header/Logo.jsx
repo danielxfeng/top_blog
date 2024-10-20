402ec4b4e47ec87e2610aed71a77bcc5d9d73070
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-20 09:32:57 +0300

    FE: Fixed Logo with test.

--- frontend/src/components/header/Logo.jsx
+++ frontend/src/components/header/Logo.jsx
@@ -5,45 +5,47 @@ import { motion } from "framer-motion";
 const Logo = () => {
   return (
     // Semantic meaning for accessibility and SEO.
-    <a href="/">
-      <div className="flex items-center gap-1" aria-label="Blog">
-        <motion.div
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.5,
-            ease: [0, 0.71, 0.2, 1.01],
-            scale: {
-              type: "spring",
-              damping: 7,
-              stiffness: 100,
-              restDelta: 0.001,
-            },
-          }}
-        >
-          <span className="font-logoMain font-extrabold text-5xl text-gradient">
-            B
-          </span>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, rotate: -90 }}
-          animate={{ opacity: 1, rotate: 0 }}
-          transition={{
-            duration: 0.3,
-            rotate: {
-              type: "spring",
-              damping: 7,
-              stiffness: 100,
-              restDelta: 0.001,
-            },
-          }}
-        >
-          <span className="font-logoSub font-extrabold text-2xl text-gradient">
-            log
-          </span>
-        </motion.div>
-      </div>
-    </a>
+    <div aria-label="Blog">
+      <a href="/">
+        <div className="flex items-center gap-1">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.5,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 7,
+                stiffness: 100,
+                restDelta: 0.001,
+              },
+            }}
+          >
+            <span className="font-logoMain font-extrabold text-5xl text-gradient">
+              B
+            </span>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, rotate: -90 }}
+            animate={{ opacity: 1, rotate: 0 }}
+            transition={{
+              duration: 0.3,
+              rotate: {
+                type: "spring",
+                damping: 7,
+                stiffness: 100,
+                restDelta: 0.001,
+              },
+            }}
+          >
+            <span className="font-logoSub font-extrabold text-2xl text-gradient">
+              log
+            </span>
+          </motion.div>
+        </div>
+      </a>
+    </div>
   );
 };
 
