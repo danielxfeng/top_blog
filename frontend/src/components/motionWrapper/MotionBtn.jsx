d2cfd64a740b21a84b4b5dbc82d122385e4e4430
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-20 13:47:13 +0300

    FE: Improved MotionBtn, Merged shadcn Button to MotionButton with default animations.

--- frontend/src/components/motionWrapper/MotionBtn.jsx
+++ frontend/src/components/motionWrapper/MotionBtn.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { motion } from "framer-motion";
 
 // The children will be scaled up when hovered.
+// This is not a real button, but a wrapper for any component.
 const MotionBtn = ({ children }) => {
   return (
     <motion.div
       className="cursor-pointer"
-      whileHover={{
-        scale: 1.3,
-        transition: { duration: 0.2 },
-        ease: [0, 0.71, 0.2, 1.01],
-      }}
+      whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
     >
       {children}
     </motion.div>
