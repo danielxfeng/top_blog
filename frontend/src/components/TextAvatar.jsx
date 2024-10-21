06702ae5286b44537e17f6dc03315f9fce9977b7
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-21 18:03:49 +0300

    FE: Some css effects.

--- frontend/src/components/TextAvatar.jsx
+++ frontend/src/components/TextAvatar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TextAvatar = ({ text }) => {
   return (
-    <div className="flex items-center justify-center w-8 h-8 rounded-full bg-secondary text-secondary-foreground font-bold text-md">
+    <div className="flex items-center justify-center w-8 h-8 rounded-full bg-secondary text-secondary-foreground text-md">
       {text ? text[0].toUpperCase() : "U"}
     </div>
   );
